ab9be43f4e41e28b352bc0f9acb7b1d8e71ec5b6
B-and-B-Game-Production <149822649+B-and-B-Game-Production@users.noreply.github.com>
2024-06-05 22:11:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Tester() {
+    alert("This action is still under development")
+}function Support() {
+    alert("This action is still under development")
+}function About() {
+    alert("This action is still under development")
+}
